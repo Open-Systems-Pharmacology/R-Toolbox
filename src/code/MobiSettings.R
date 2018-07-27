bd1ed46a5fbe6e4b0d0efa328a735c59d07dd2da
Michael Sevestre <michael@design2code.ca>
2018-07-27 07:42:09 -0400

    Fixes #40 64 bit setup supporting R 3.5 (#96)
    * Fixes #40 64 bit setup supporting R 3.5
    
    * Update setup
    
    * Update setup file
    
    * Ensure that mobi settings are searched in x64. Also Fixes #98

--- src/code/MobiSettings.R
+++ src/code/MobiSettings.R
@@ -1,19 +1,19 @@
 
 MobiSettings <- function(libraryName = "MoBiToolboxForR")
 {
-  pathes <- .libPaths()
-  for (i in 1:length(pathes)) {
-    libpath <- file.path(pathes[i], libraryName, "libs", "i386")
+  paths <- .libPaths()
+  for (i in 1:length(paths)) {
+    libpath <- file.path(paths[i], libraryName, "libs", "x64");
     
     if (file.exists(libpath)) {
-    
+      
       Sys.setenv(path = paste(libpath, Sys.getenv("path"), sep =";"))
-      MobiSettings <- list("SimModelSchema" = paste(libpath,"\\OSPSuite.SimModel.xsd", sep=""),
-                           "SimModelComp" = paste(libpath, "\\OSPSuite_SimModelComp.xml", sep=""),
-                           "RInterface" = paste(libpath,"\\DCIR6_1.dll", sep=""));
+      return(list("SimModelSchema" = paste(libpath,"/OSPSuite.SimModel.xsd", sep=""),
+                  "SimModelComp" = paste(libpath, "/OSPSuite_SimModelComp.xml", sep=""),
+                  "RInterface" = paste(libpath,"/DCIR6_1.dll", sep="")));
     }
   }
-
-	return(MobiSettings) 
+  
+  stop(paste("Library '", libraryName, "' not found in libPaths()!", sep=""));
 }
 
